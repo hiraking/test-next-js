1080b5b4089ff7acd877beb95cb07f1b71dbdaca
hiraki <hirakitaito@gmail.com>
2023-05-24 02:09:51 +0900

    create layout.tsx for async

---
+++ app/async/layout.tsx
@@ -0,0 +1,20 @@
+import '../globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'async VS sync',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
